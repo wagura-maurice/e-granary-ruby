c39a9babb79431023c14e4783a998c3ea0e5e1be
Wagura Maurice <wagura465@gmail.com>
2019-01-18 20:34:24 +0300

    update root to loading customers index function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'customer/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# get '/' => redirect('customer/index')
+	# get 'customer/index'
+	get '/', to: 'customer#index', as: 'customer'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
