ab841741fbb4ee96e4e4c3909db64d187d35e347
Wagura Maurice <wagura465@gmail.com>
2019-01-19 00:38:18 +0300

    boostrap ui refined

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 	# get '/' => redirect('customer/index')
 	# get 'customer/index'
 	get '/', to: 'customer#index', as: 'customer'
-	post '/create', to: 'customer#create', as: 'customer_create'
-	get '/thank_you', to: 'customer#thank_you', as: 'thank_you'
+	post '/', to: 'customer#create', as: 'customer_create'
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
